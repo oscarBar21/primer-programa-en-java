209f218362962a35997ea7baeeae0fe6885d461a
oscar <oscarbarragan31@gmail.com>
2020-10-08 12:14:07 +0200

    añadi mi localidaz de residencia actual y el so que utilizo

--- Prueba.java
+++ Prueba.java
@@ -4,6 +4,7 @@ public class Prueba {
 		
 		System.out.println("Oscar Barragan Mateos");
 		System.out.println("Leon, 22/05/1998");
+		System.out.println("Santa Maria del Paramo, Windows");
 
 	 }
 
